398d864097f38e03a0ad1d3f63a9880fe715d24b
Zyldzkie <88105329+Zyldzkie@users.noreply.github.com>
2024-12-16 05:08:09 +0800

    [Feature] Registration

---
+++ TaskManagement/Migrations/20241215205731_MigrationName.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagement.Migrations
+{
+    public partial class MigrationName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_Roles_RoleID",
+                table: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Roles",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeNo",
+                table: "User");
+
+            migrationBuilder.RenameTable(
+                name: "Roles",
+                newName: "Role");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Comments",
+                table: "Task",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Task",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApprovedBy",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Feedback",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Project",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RejectedBy",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SubmittedBy",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Role",
+                table: "Role",
+                column: "RoleID");
+
+            migrationBuilder.CreateTable(
+                name: "Feedback",
+                columns: table => new
+                {
+                    FeedBackId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProjectID = table.Column<int>(type: "int", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Comments = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    SubmittedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedback", x => x.FeedBackId);
+                    table.ForeignKey(
+                        name: "FK_Feedback_Project_ProjectID",
+                        column: x => x.ProjectID,
+                        principalTable: "Project",
+                        principalColumn: "ProjectID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedback_ProjectID",
+                table: "Feedback",
+                column: "ProjectID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_Role_RoleID",
+                table: "User",
+                column: "RoleID",
+                principalTable: "Role",
+                principalColumn: "RoleID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_Role_RoleID",
+                table: "User");
+
+            migrationBuilder.DropTable(
+                name: "Feedback");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Role",
+                table: "Role");
+
+            migrationBuilder.DropColumn(
+                name: "Comments",
+                table: "Task");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Task");
+
+            migrationBuilder.DropColumn(
+                name: "ApprovedBy",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "Feedback",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "RejectedBy",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "SubmittedBy",
+                table: "Project");
+
+            migrationBuilder.RenameTable(
+                name: "Role",
+                newName: "Roles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeNo",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Roles",
+                table: "Roles",
+                column: "RoleID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_Roles_RoleID",
+                table: "User",
+                column: "RoleID",
+                principalTable: "Roles",
+                principalColumn: "RoleID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
